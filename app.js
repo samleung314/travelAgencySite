9d46197cb4ea8f2370f11998dc371c50d3660a2d
samleung314 <samleung314@gmail.com>
2018-04-08 22:15:33 -0400

    setup express

--- app.js
+++ app.js
@@ -38,4 +38,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+// start server
+app.listen(80, () => console.log('Travel Agency listening on port 80!'))
+
 module.exports = app;
