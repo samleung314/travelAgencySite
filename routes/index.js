3d7e8034d72e4a43512cacb5c1f83c717dbcb1a9
kevchang <kevchang@cs.stonybrook.edu>
2018-04-27 21:51:45 -0400

    Added form and made a new temp page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'My Travel Agency' });
 });
+
+router.get('/main.hbs', function(req, res, next) {
+  res.render('main', { title: 'My Travel Agency' });
+});
+
 
 module.exports = router;
