c305d3a2f8f8f15139382152b9f2d03116f66bf5
kevchang <kevchang@cs.stonybrook.edu>
2018-05-02 03:57:06 -0400

    Added login/registration screens

--- routes/database.js
+++ routes/database.js
@@ -12,15 +12,33 @@ var con = mysql.createConnection({
   
 
 /* GET home page. */
-router.post('/', function(req, res, next) {
+router.post('/register', function(req, res, next) {
     var p= req.body;
-    var values = "(0,'"+p.firstname+"', '"+p.lastname+"', '"+ p.gender +"', "+p.age+")";
-    var sql = "INSERT INTO passenger (groupID, fName, lName, gender, age) VALUES"+values;
+    var values = "(0,'"+p.firstname+"', '"+p.lastname+"', "+ p.age +", '"+p.gender+"', '"+p.pass+"')";
+    var sql = "INSERT INTO passenger (groupID, fName, lName, age, gender,pass) VALUES"+values;
     
     con.query(sql, function (err, result) {
         if (err) throw err;
         console.log("1 record inserted");
     });
+    res.render('login', { title: 'My Travel Agency' });
+});
+
+router.post('/login', function(req, res, next) {
+    /*
+    var p= req.body;
+    console.log(p.pid);
+    console.log(p.pass);
+    "SELECT name, address"
+    "FROM customers"
+    "WHERE password = "+p.pass
+    var sql = "";
+    
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("1 record inserted");
+    });
+    */
     res.render('login', { title: 'My Travel Agency' });
 });
 
