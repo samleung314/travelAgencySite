c6e012e3af82a11710018caba2b9db79c435d913
unknown <stebbun@gmail.com>
2018-05-07 17:15:05 -0400

    Accommodation booking

--- routes/database.js
+++ routes/database.js
@@ -57,7 +57,7 @@ router.post('/login', function(req, res, next) {
   var sql = "SELECT pass,fName FROM TravelAgency.Passenger WHERE email = '"+p.email +"'";
   con.query(sql, function (err, result) {
     if(result[0].pass == null){
-        res.render('login', { title: 'My Travel Agency', logged: "Login",message: 'Invalid Credentials', layout: "nonuser"});  
+        res.render('login', { title: 'My Travel Agency', logged: "Login",message: 'Invalid Credentials', layout: "nonuser"});
     }
     if(result.length == 1 && result[0].pass == p.pass){
       res.cookie('name',result[0].fName);
@@ -116,10 +116,15 @@ router.post('/accommodation', function(req, res, next) {
   }else{
     amen = "1 = 1";
   }
-  var sql = "SELECT Address, RoomNumber, Rate FROM Accommodation INNER JOIN (SELECT ID AS hotel FROM Amenities WHERE " + amen + ") AS T1 ON Accommodation.ID = hotel INNER JOIN (SELECT * FROM Room) AS T2 ON HotelID = Accommodation.ID INNER JOIN (SELECT CityID, City AS CityName FROM Location) AS T3 ON City = CityID WHERE AType = '" + p.type + "' AND CityName = '" + p.city + "' ORDER BY Address ASC";
-  console.log(sql);
+  var sql = "SELECT ID, Address, RoomNumber, Rate FROM Accommodation INNER JOIN (SELECT ID AS hotel FROM Amenities WHERE " + amen + ") AS T1 ON Accommodation.ID = hotel INNER JOIN (SELECT * FROM Room) AS T2 ON HotelID = Accommodation.ID INNER JOIN (SELECT CityID, City AS CityName FROM Location) AS T3 ON City = CityID WHERE AType = '" + p.type + "' AND CityName = '" + p.city + "' ORDER BY Address ASC";
+  var year = p.date.substr(0,4);
+  var day = p.date.substr(5,2);
+  var month = p.date.substr(8,2);
+  if(!(/^\d+$/.test(year) && /^\d+$/.test(day) && /^\d+$/.test(month))){
+      p.date = "2000-01-01"; //default date
+  }
   con.query(sql, function (err, result) {
-    res.render('foundrooms', {title: 'My Travel Agency', rooms: result, city: p.city, atype: p.type, gym: p.gym, lounge: p.lounge, pool: p.pool});
+    res.render('foundrooms', {title: 'My Travel Agency', rooms: result, city: p.city, atype: p.type, gym: p.gym, lounge: p.lounge, pool: p.pool, checkin: p.date});
   });
 });
 
@@ -143,10 +148,11 @@ router.post('/bookcruise', function(req, res, next) {
 
 router.post('/bookroom', function(req, res, next) {
   var p= req.body;
-  var sql = "";
-  console.log(p);
+  var room = p.book.substr(0,3);
+  var hotelid = p.book.substr(3);
+  var sql = "UPDATE Room SET OccupiedFrom = '" + p.date + "' WHERE RoomNumber = '" + room + "' AND HotelID = '" + hotelid + "'"; //add occupiedby groupid
   con.query(sql, function (err, result) {
-    //res.render('foundrooms', {title: 'My Travel Agency', message: "Your rooms has been booked!"});
+    res.render('foundrooms', {title: 'My Travel Agency', message: "Your room has been booked!"});
   });
 });
 
@@ -156,7 +162,7 @@ router.post('/addPassenger', function(req, res, next) {
     var n2 = p.lName;
     var n3 = p.age;
     var n4 = p.email;
-    
+
     var entity = [];
     entity.push(p.email);
     entity.push(null);
@@ -182,7 +188,7 @@ router.post('/addPassenger', function(req, res, next) {
             res.redirect('/')
         }
     });
-    
+
 });
 
 router.post('/updateGroup', function(req, res, next) {
